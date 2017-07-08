ec1a3ac3c09a289f6ddd3c470e36627010870921
Piotr Baniukiewicz <P.Baniukiewicz@warwick.ac.uk>
2017-07-08 22:32:05 +0100

    Added min max ranges

--- PrepareData.py
+++ PrepareData.py
@@ -12,7 +12,7 @@ allBounds = []  # will store bounds dictionary
 allCentroids = []  # centroids in order of bounds
 
 # folder to scan
-fileList = scanFolder("/home/baniuk/Documents/Kay-copy/FLU+DIC")
+fileList = scanFolder("c:\\Users\\baniu\\OneDrive\\CellExtractor_Test")
 
 # iterate over QCONF files and extract bounds
 for qconf in fileList:
@@ -27,11 +27,15 @@ for b in allBounds:
     bounds.append([b["x"], b["y"], b["width"], b["height"]])
 bounds = np.array(bounds)  # convert to numpy
 
-# compute basic stats - 1st 2nd and 3rd quartile
+# %% compute basic stats - 1st 2nd and 3rd quartile
 med = np.percentile(bounds[:, (2, 3)], (25, 50, 75), axis=0)
 pmed = pandas.DataFrame(
     med, columns=['Width', 'Height'], index=['25', '50', '75'])
 pmed.plot.box()
 print(pmed)
-
 plt.show()
+# %% Compute range of data
+ranges = pandas.DataFrame(pmed.min(), columns=['min'])
+ranges['max'] = pandas.DataFrame(pmed.max(), columns=['max'])
+ranges = ranges.T  # transpose to have same orientation as med
+print(ranges)
